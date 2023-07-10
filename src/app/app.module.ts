1b23c43ea948b631f39f727f2ab287871a8d2daa
Michael <michael@codetropolis.com>
2023-07-09 20:33:28 -0500

    Added header, post-list, and post.service. Also added other Angular Material components.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,16 @@ import { PostCreateComponent } from './posts/post-create/post-create.component';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { PostListComponent } from './posts/post-list/post-list.component';
+import { HeaderComponent } from './header/header.component';
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    PostCreateComponent,
+    PostListComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
@@ -18,7 +24,9 @@ import { MatCardModule } from '@angular/material/card';
     BrowserAnimationsModule,
     MatInputModule,
     MatButtonModule,
-    MatCardModule
+    MatCardModule,
+    MatToolbarModule,
+    MatExpansionModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
