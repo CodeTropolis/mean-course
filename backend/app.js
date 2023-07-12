99d7a0bacd0a9b8dcdfdc07aac23d025cb5081f7
Michael <michael@codetropolis.com>
2023-07-12 03:12:33 -0500

    Add api/posts end point middleware.

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,13 @@ const express = require('express');
 const app = express();
 
 // Middleware
-app.use((req, res, next) => {
-    console.log('First middleware');
-    next();
-});
 
-app.use((req, res, next) => {
-    res.send('Hello from Express!')
+app.use("/api/posts",(req, res, next) => {
+    const posts = [
+        {id: 'e45trey', title: 'The first post', content: 'The first post content (from server)'},
+        {id: 'y78u9io', title: 'The second post', content: 'The second post content (from server)'},
+    ]
+    res.status(200).json({message: 'Successful retrival of posts!', posts:posts});
 });
 // Utilize this in root/server.js.
 module.exports = app;
